8849d37a95732894fd67dc353641ac4ac9ac0c65
brandonlglzArch <brandonlglz000@gmail.com>
2024-04-26 10:23:46 -0600

    vie 26 abr 2024 10:23:46 CST

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -7,12 +7,12 @@ function TodoList() {
     const {filtredT, filtredSC, filtredC, loading, searchedTodos, deleteTodo, completeTodo, dark, appLanguaje} = React.useContext(TodoContext);
     return (
         <div className={"todo_list_container " + (dark && 'darkShadow')}>
-            {appLanguaje == 'es' && <p onClick={filtredT} id="slider_1" className={"focus_slider slider " + (dark && 'darkBackground')}>Todas</p>}
-            {appLanguaje == 'en' && <p onClick={filtredT} id="slider_1" className={"focus_slider slider " + (dark && 'darkBackground')}>All</p>}
-            {appLanguaje == 'es' && <p onClick={filtredSC} id="slider_2" className={dark && 'darkBackground ' }>Sin completar</p>}
-            {appLanguaje == 'en' && <p onClick={filtredSC} id="slider_2" className={dark && 'darkBackground'}>Not completed</p>}
-            {appLanguaje == 'es' && <p onClick={filtredC} id="slider_3" className={dark && 'darkBackground'}>Completadas</p>}
-            {appLanguaje == 'en' && <p onClick={filtredC} id="slider_3" className={dark && 'darkBackground'}>Completed</p>}
+            {appLanguaje === 'es' && <p onClick={filtredT} id="slider_1" className={"focus_slider slider " + (dark && 'darkBackground')}>Todas</p>}
+            {appLanguaje === 'en' && <p onClick={filtredT} id="slider_1" className={"focus_slider slider " + (dark && 'darkBackground')}>All</p>}
+            {appLanguaje === 'es' && <p onClick={filtredSC} id="slider_2" className={dark && 'darkBackground' }>Sin completar</p>}
+            {appLanguaje === 'en' && <p onClick={filtredSC} id="slider_2" className={dark && 'darkBackground'}>Not completed</p>}
+            {appLanguaje === 'es' && <p onClick={filtredC} id="slider_3" className={dark && 'darkBackground'}>Completadas</p>}
+            {appLanguaje === 'en' && <p onClick={filtredC} id="slider_3" className={dark && 'darkBackground'}>Completed</p>}
             <ul>
                 {loading && <div className="wrapper">
                                     <div className="space">
